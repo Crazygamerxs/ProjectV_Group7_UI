655027c21f3af59f6885199b167b031a9426b860
praiseprince <praiseprince35@gmail.com>
2024-11-06 02:00:31 -0500

    added survivor

--- ProjectV_Group7_UI_API/Program.cs
+++ ProjectV_Group7_UI_API/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<CriticalNotificationService>();
+builder.Services.AddSingleton<SurvivorInformationService>(); // Register SurvivorInformationService here
 builder.Services.AddSingleton<AddNotificationModal>();
 
 var app = builder.Build();
@@ -19,14 +20,11 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
 
 app.MapBlazorHub();
